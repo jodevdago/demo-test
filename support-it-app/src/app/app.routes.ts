0a44c72f0787c23f84644a6d66e39603c5eb439f
jodevdago <jogasy.rabefialy@gmail.com>
2024-09-21 10:56:22 +0300

    setting up app

---
+++ support-it-app/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+]
