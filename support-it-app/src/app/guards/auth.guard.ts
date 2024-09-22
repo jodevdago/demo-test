b401c366c8d2ac5335fd4cb25194d785b1995a23
jodevdago <jogasy.rabefialy@gmail.com>
2024-09-22 17:21:36 +0300

    feat: add guard

---
+++ support-it-app/src/app/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import { inject } from '@angular/core';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { catchError, from, map, Observable, of } from 'rxjs';
+
+export const AuthGuard = () => {
+  const auth = inject(Auth);
+  const firestore = inject(Firestore);
+  const router = inject(Router);
+
+  return new Observable<boolean>((observer) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userDocRef = doc(firestore, `users/${user.uid}`);
+        from(getDoc(userDocRef))
+          .pipe(
+            map((userDoc) => {
+              if (userDoc.exists()) {
+                const userData = userDoc.data();
+                return userData && userData['auth'] === true;
+              } else {
+                return false;
+              }
+            }),
+            catchError((error) => {
+              console.error('Error retrieving Firestore document', error);
+              return of(false);
+            })
+          )
+          .subscribe((authValid) => {
+            console.log(authValid);
+            if (authValid) {
+              observer.next(true);
+            } else {
+              router.navigate(['./unauthorized']);
+              observer.next(false);
+            }
+            observer.complete();
+          });
+      } else {
+        router.navigate(['./login']);
+        observer.next(false);
+        observer.complete();
+      }
+    });
+  });
+};
