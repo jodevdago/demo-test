6d587b5c9cb755e3281d2fcd9a95ea6bb4669d4d
jodevdago <jogasy.rabefialy@gmail.com>
2024-09-23 03:15:02 +0300

    fix: add methods to manage data

--- support-it-app/src/app/services/tickets.service.ts
+++ support-it-app/src/app/services/tickets.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketsService {
-
   constructor(private firestore: Firestore) {}
 
   getTickets(): Observable<any[]> {
     const usersCollection = collection(this.firestore, 'tickets');
     return collectionData(usersCollection, { idField: 'id' });
+  }
+
+  createDocument(data: any) {
+    const usersCollection = collection(this.firestore, 'tickets');
+    return from(addDoc(usersCollection, data));
+  }
+
+  deleteDocument(id: string): Observable<void> {
+    const ticketDocRef = doc(this.firestore, `tickets/${id}`);
+    return from(deleteDoc(ticketDocRef));
+  }
+
+  updateDocument(userId: string, data: any): Observable<void> {
+    const userDocRef = doc(this.firestore, `tickets/${userId}`);
+    return from(updateDoc(userDocRef, data));
   }
 }
